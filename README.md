8b7263f94e8750af3abb701775cfb03869a74885
aaron mayhew <aaronmayhew@aarons-MacBook-Pro-2.local>
2018-08-04 21:33:39 -0500

    cant get sql files to attack

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bamazon
 Homework 11
+
+So im doing the minnimum requirement for the assignment due to time constraints, i hope to come back to it next week and work on the more challenging aspects of it once im moved in to the new place. 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,13 +7,13 @@ var connection = mysql.createConnection({
     user: "root",
     password: "bellebelle",
     database: "bamazon"
-})
+});
 
 connection.connect(function(err){
     if (err) throw err;
     console.log("connection successful!");
     makeTable();
-})
+});
 
 var makeTable = function(){
     connection.query("SELECT * FROM products", function(err,res){
@@ -22,5 +22,55 @@ var makeTable = function(){
         res[i].departmentname+" || "+res[i].price+" || "+res[i].
         stockquantity+"\n");
         }
-    })
-}
+    promptCustomer(res);
+    });
+};
+
+var promptCustomer = function(res){
+    inquirer.prompt([{
+        type:'input',
+        name:'choice',
+        message:"What you buy? [Quit with Q]"
+    }]).then(function(answer){
+        var correct = false;
+        if(answer.choice.toUpperCase()=="Q"){
+            process.exit();
+        }
+        for(var i=0; i<res.length;i++){
+            if(res[i].productname==answer.choice){
+                correct=true;
+                var product=answer.choice;
+                var id=i;
+                inquirer.prompt({
+                    type:"input",
+                    name:"quant",
+                    message:"How many you buy?",
+                    validate: function(value){
+                        if(isNaN(value)==false){
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function(answer){
+                    if((res[id].stockquantity-answer.quant)>0){
+                      connection.query("UPDATE products SET
+                        stockquantity='"+(res[id].stockquantity-
+                        answer.quant)+"' WHERE productname='"+product
+                        +"'", function(err,res2){
+                            console.log("You Buy!");
+                            makeTable();
+                        });
+                    } else {
+                        console.log("You no Buy, try again!");
+                        promptCustomer(res);
+                    }
+                });
+            }
+        }
+        if(i=res.length && correct==false){
+            console.log("We No Have!! /em spits at your feet");
+            promptCustomer(res);
+        }
+    });
+};
