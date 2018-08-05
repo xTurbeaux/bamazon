21fb79c8fe0b6364af66daed69a4c07683c20f87
aaron mayhew <aaronmayhew@aarons-MacBook-Pro-2.local>
2018-08-04 20:44:30 -0500

    got stuff up and running, now for js

---
+++ bamazonCustomer.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host:"localhost",
+    port: 3306,
+    user: "root",
+    password: "bellebelle",
+    database: "bamazon"
+})
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connection successful!");
+    makeTable();
+})
+
+var makeTable = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        for(var i=0; i<res.length; i++){
+            console.log(res[i].itemid+" || "+res[i].productname+" || "+
+        res[i].departmentname+" || "+res[i].price+" || "+res[i].
+        stockquantity+"\n");
+        }
+    })
+}
